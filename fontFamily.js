6938265c14b2e51db11e3f5eecad24acd634ee82
Carsten Schäfer <carsten.schaefer@g-tac.de>
2015-02-21 10:00:50 +0100

    Update fontFamily.js

--- fontFamily.js
+++ fontFamily.js
@@ -2,56 +2,56 @@ if (!RedactorPlugins) var RedactorPlugins = {};
 
 (function ($) {
   RedactorPlugins.fontFamily = function () {
-     var arrows = [37,38,39,40];
-  
-     return {
-        init: function(){
-            var fonts = [ 'Arial', 'Helvetica', 'Georgia', 'Times New Roman', 'Monospace' ];
-            var dropdown = {};
+    var arrows = [37, 38, 39, 40];
 
-            options = this.opts.fontFamily || { defaultFont: 'Arial' };
+    return {
+      init: function () {
+        var fonts = ['Arial', 'Helvetica', 'Georgia', 'Times New Roman', 'Monospace'];
+        var dropdown = {};
 
-            $.each(fonts, function(i, s)
-            {
-                dropdown['s' + i] = { 
-                    title: s, 
-                    func: function(e) { 
-                        this.fontFamily.onSelect(s);
-                    }};
-            });
+        options = this.opts.fontFamily || { defaultFont: 'Arial' };
 
-            var button = this.button.add('fontList', "Font");
-			button[0].innerHTML = options.defaultFont;
-			$(this.button.get('fontList')[0]).css('font-family', options.defaultFont);
-			this.core.getElement()[0].style.fontFamily = options.defaultFont;
-            this.button.addDropdown(button, dropdown);
-			
-			this.opts.clickCallback = function(){
-                this.fontFamily.caretChanged(this);
-            };
-            
-            this.opts.keyupCallback = function(e){
-                var key = e.keyCode || e.which;
-                if(arrows.indexOf(key) === -1) return;
-                
-                this.fontFamily.caretChanged(this);
-            };
-        },
-        onSelect: function(font){
-            this.button.get('fontList')[0].innerHTML = font;
-			$(this.button.get('fontList')[0]).css('font-family', font);
-            this.inline.format('span', 'style', 'font-family:' + font + ';');
+        $.each(fonts, function (i, s) {
+          dropdown['s' + i] = {
+            title: s,
+            func: function (e) {
+              this.fontFamily.onSelect(s);
+            }
+          };
+        });
 
-        },
-        caretChanged: function(t){
-            var node = t.sel.focusNode.parentElement;
-            var font = window.getComputedStyle( node, null ).getPropertyValue( 'font-family' );
+        var button = this.button.add('fontList', "Font");
+        button[0].innerHTML = options.defaultFont;
+        $(this.button.get('fontList')[0]).css('font-family', options.defaultFont);
+        this.core.getElement()[0].style.fontFamily = options.defaultFont;
+        this.button.addDropdown(button, dropdown);
 
-            font = font.replace(/['"]+/g, '');
+        this.opts.clickCallback = function () {
+          this.fontFamily.caretChanged(this);
+        };
 
-            this.button.get('fontList')[0].innerHTML = font;
-			$(this.button.get('fontList')[0]).css('font-family', font);
-        }
-	}
-    };
+        this.opts.keyupCallback = function (e) {
+          var key = e.keyCode || e.which;
+          if (arrows.indexOf(key) === -1) return;
+
+          this.fontFamily.caretChanged(this);
+        };
+      },
+      onSelect: function (font) {
+        this.button.get('fontList')[0].innerHTML = font;
+        $(this.button.get('fontList')[0]).css('font-family', font);
+        this.inline.format('span', 'style', 'font-family:' + font + ';');
+
+      },
+      caretChanged: function (t) {
+        var node = t.sel.focusNode.parentElement;
+        var font = window.getComputedStyle(node, null).getPropertyValue('font-family');
+
+        font = font.replace(/['"]+/g, '');
+
+        this.button.get('fontList')[0].innerHTML = font;
+        $(this.button.get('fontList')[0]).css('font-family', font);
+      }
+    }
+  };
 })(jQuery);
